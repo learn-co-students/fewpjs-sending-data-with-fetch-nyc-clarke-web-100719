1689aeadb6084187321584710a5c33f8bcc8fbf7
Barbara <muesingb@gmail.com>
2019-11-25 18:56:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your code here
+function submitData(username, email) {
+    return fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: username,
+            email: email
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.id)
+        document.body.innerHTML = data.id
+    })
+    .catch(err => document.body.innerHTML = err.message)
+};
